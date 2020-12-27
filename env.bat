1c189a62003e662b21da9b544563c806dcfb3b6e
webnet <muhommadraahim1@gmail.com>
2020-12-27 23:08:28 +0500

    run as administrator
    run as administrator

---
+++ env.bat
@@ -0,0 +1,10 @@
+@echo off
+setx /M ANDROID_HOME "C:\cordova\Android\android-sdk"
+setx /M ANDROID_SDK_ROOT "C:\cordova\Android\android-sdk"
+setx /M ANDROID_SDK_TOOLS "C:\cordova\Android\android-sdk\tools"
+setx /M ANDROID_SDK_PLATFORMTOOLS "C:\cordova\Android\android-sdk\platform-tools"
+setx /M ANDROID_SDK_BUILDTOOLS "C:\cordova\Android\android-sdk\build-tools\29.0.2"
+setx /M apache-ant "C:\cordova\apache-ant-1.10.7\bin"
+setx /M gradle "C:\cordova\gradle-6.1\bin;"
+setx /M java "C:\cordova\Java\jdk1.8.0_241\bin"
+pause
