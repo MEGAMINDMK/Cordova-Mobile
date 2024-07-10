f015d07ebaa99469267816d2e0e6fe053d35f7f4
webnet <muhommadraahim1@gmail.com>
2024-07-10 21:40:10 +0500

    Update env.bat

--- env.bat
+++ env.bat
@@ -5,6 +5,6 @@ setx /M ANDROID_SDK_TOOLS "C:\cordova\Android\android-sdk\tools"
 setx /M ANDROID_SDK_PLATFORMTOOLS "C:\cordova\Android\android-sdk\platform-tools"
 setx /M ANDROID_SDK_BUILDTOOLS "C:\cordova\Android\android-sdk\build-tools\29.0.3"
 setx /M apache_ant "C:\cordova\apache_ant_1.10.14\bin"
-setx /M gradle "C:\cordova\gradle_8.8\bin"
-setx /M JAVA_HOME "C:\cordova\Java\jdk_1.8\bin"
+setx /M gradle "C:\cordova\gradle-8.8\bin"
+setx /M JAVA_HOME "C:\cordova\Java\jdk1.8\bin"
 pause
